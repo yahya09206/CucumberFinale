2d5f46282f5bfa6b8bf6489c0b6bd0b6dc216819
Yahya <yahyahussein91@gmail.com>
2024-10-25 15:22:38 -0700

    set up hooks class with proper annonatations

---
+++ src/test/java/com/yahya/utility/Hooks.java
@@ -0,0 +1,26 @@
+package com.yahya.utility;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+import java.time.Duration;
+
+public class Hooks {
+
+    /**
+     * Can set up a hook class that contains methods that run each scenario and after each scenario
+     * or even when you implement hooks
+     * allows you to run certain code before and after each scenario that is tagged with certain tags
+     */
+
+    @Before
+    public void setupDriver(){
+
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @After
+    public void tearDown(){
+        Driver.closeBrowser();
+    }
+}
