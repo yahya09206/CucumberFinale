0e0b5b6e2333d21d9d6e2231a93b0ee5e85f7ecd
Yahya <yahyahussein91@gmail.com>
2024-10-25 09:58:44 -0700

    verified feature for logging in is working

---
+++ src/test/java/com/yahya/step_definitions/WebOrderLoginStepDef.java
@@ -0,0 +1,34 @@
+package com.yahya.step_definitions;
+
+import com.yahya.pages.WLogin;
+import com.yahya.utility.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WebOrderLoginStepDef {
+
+    WLogin login;
+
+    @Given("we are at the web order login page")
+    public void we_are_at_the_web_order_login_page() {
+
+
+        login = new WLogin();
+        login.goTo();
+//        login.login("Test", "test");
+    }
+    @When("we provide valid credentials")
+    public void we_provide_valid_credentials() {
+
+        login.login("Tester", "test");
+    }
+    @Then("we should see the all orders page")
+    public void we_should_see_the_all_orders_page() {
+
+        assertEquals("Web Orders", Driver.getDriver().getTitle());
+    }
+}
