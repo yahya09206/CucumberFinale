cdab2cecb5b338ffd79d90321c9d9b821ea3d35f
Yahya <yahyahussein91@gmail.com>
2024-10-25 15:30:50 -0700

    scenario for when a login fails

--- src/test/java/com/yahya/utility/Hooks.java
+++ src/test/java/com/yahya/utility/Hooks.java
@@ -11,6 +11,7 @@ public class Hooks {
      * Can set up a hook class that contains methods that run each scenario and after each scenario
      * or even when you implement hooks
      * allows you to run certain code before and after each scenario that is tagged with certain tags
+     * Don't need to use the extends keyword because io.cucumber.java already knows
      */
 
     @Before
